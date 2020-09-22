9d49e4e048887eb1b4e005381acdf4021bc20b27
Georgijs Ventura <ventura55g@gmail.com>
2020-09-22 23:36:29 +0100

    caloriestrackerjs

--- app.js
+++ app.js
@@ -294,6 +294,14 @@ const App = (function(ItemCtrl, UICtrl){
     // Update UI
     UICtrl.updateListItem(updatedItem);
 
+    // Get total calories
+    const totalCalories = ItemCtrl.getTotalCalories();
+      
+    // Add total calories to UI
+    UICtrl.showTotalCalories(totalCalories);
+
+    UICtrl.clearEditStatus();
+
     e.preventDefault();
   }
 
@@ -316,6 +324,7 @@ const App = (function(ItemCtrl, UICtrl){
 
       // Get total calories
       const totalCalories = ItemCtrl.getTotalCalories();
+
       // Add total calories to UI
       UICtrl.showTotalCalories(totalCalories);
 
